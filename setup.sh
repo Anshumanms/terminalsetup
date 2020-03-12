629babc2dd95b7db30faef10c62e59b26cc6a4ce
datchina <datchinamurthy@gmail.com>
2020-03-12 16:06:42 +0530

    vim_ide installation script

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Creating soft links to vim_ide
+ln -s /workspace/sw/dkaliyan/dk/vim_ide/vimrc ~/.vimrc
+ln -s /workspace/sw/dkaliyan/dk/vim_ide/cscope_maps.vim ~/cscope_maps.vim
+ln -s /workspace/sw/dkaliyan/dk/vim_ide/fzf_maps.vim ~/fzf_maps.vim
