c41ccdb51b65871f19998f65826b14ea4473e17b
datchina <datchinamurthy@gmail.com>
2020-03-12 16:33:28 +0530

    Fixing full path in soft link creation

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Creating soft links to vim_ide
-ln -s ./vimrc ~/.vimrc
-ln -s ./cscope_maps.vim ~/cscope_maps.vim
-ln -s ./fzf_maps.vim ~/fzf_maps.vim
-ln -s ./tmux.conf ~/.tmux.conf
+ln -s $(pwd)/vimrc ~/.vimrc
+ln -s $(pwd)/cscope_maps.vim ~/cscope_maps.vim
+ln -s $(pwd)/fzf_maps.vim ~/fzf_maps.vim
+ln -s $(pwd)/tmux.conf ~/.tmux.conf
