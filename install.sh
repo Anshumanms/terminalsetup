281c9d799324713255e58cc9ff4ade4390698ead
datchina <datchinamurthy@gmail.com>
2020-03-12 17:29:16 +0530

    Adding installation and uninstallation scripts

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ ln -s $(pwd)/vimrc ~/.vimrc
 ln -s $(pwd)/cscope_maps.vim ~/cscope_maps.vim
 ln -s $(pwd)/fzf_maps.vim ~/fzf_maps.vim
 ln -s $(pwd)/tmux.conf ~/.tmux.conf
+ln -s $(pwd)/screenrc ~/.screenrc
